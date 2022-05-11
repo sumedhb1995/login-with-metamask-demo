ae36efce875e97eb2b21fac8223807a5266c2825
Sumedh Bhattacharya <sumedhb1995@gmail.com>
2022-05-10 17:03:24 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+yarn install # Install root dependencies (for TS & linting in your IDE)
+cd packages/backend && yarn install # Install backend packages
+cd ../frontend && yarn install # Install frontend packages
+cd ../.. # Go back to root folder
